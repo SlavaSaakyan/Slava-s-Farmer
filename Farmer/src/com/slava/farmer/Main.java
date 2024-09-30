a8b844cfaca7cc63b8ee82112e38cc09a47f1c30
Вячеслав <saakyan1552@mail.ru>
2024-09-30 23:04:40 +0400

    First project commit

---
+++ Farmer/src/com/slava/farmer/Main.java
@@ -0,0 +1,10 @@
+package com.slava.farmer;
+
+/**
+ * This is Slava Farmer project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Slava Farmer project");
+    }
+}
